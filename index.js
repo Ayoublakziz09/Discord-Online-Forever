7b3ff58787375efe0c1147e993d3060072f57786
Ayub LZ <93094124+Ayoublakziz09@users.noreply.github.com>
2026-01-28 21:04:49 +0100

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Eris = require("eris");
 const keep_alive = require('./keep_alive.js')
 
 // Replace TOKEN with your bot account's token
-const bot = new Eris(process.env.token);
+const bot = new Eris(process.env.NDQ5Nzc4MTg1NjM5NTU5MTY4.GCZ-JY.jDarSeqzYK5lcvsJ2WRmubnkrrMzfuLKnl9hzg");
 
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
